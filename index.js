9d3294373f60195a5ab20d02505cce9a06558537
jamesjara <jamesjara@gmail.com>
2018-02-18 23:50:13 -0600

    adding missing methods and eslint

--- index.js
+++ index.js
@@ -1,134 +1,25 @@
-import React, { Component } from 'react';
-import AWS from 'aws-sdk/dist/aws-sdk-react-native';
-import { AWSIoTData } from './aws-iot-device-sdk-js-react-native.js';
-
-/**
- * Use this class as a React native component
- *
- * @param newConfig
- * @returns {{}}
- * @constructor
- */
-class AWSIoTMQTT extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.type = props.type === 'shadow' ? 'thingShadow' : 'device';
-        this.service = null;
-        this.registry = {};
-    }
-    
-    componentDidMount() {
-        const config = {
-            //
-            // Set the AWS region we will operate in.
-            //
-            region: this.props.region,
-            host: this.props.host,
-            //
-            // Connect via secure WebSocket
-            //
-            protocol: 'wss',
-            //
-            // Set the maximum reconnect time to 8 seconds; this is a browser application
-            // so we don't want to leave the user waiting too long for reconnection after
-            // re-connecting to the network/re-opening their laptop/etc...
-            //
-            maximumReconnectTimeMs: 8000,
-            //
-            // Enable console debugging information (optional)
-            //
-            debug: false,
-            //
-            // IMPORTANT: the AWS access key ID, secret key, and sesion token must be
-            // initialized with empty strings.
-            //
-            accessKeyId: '',
-            secretKey: '',
-            sessionToken: ''
-        };
-        
-        if (this.props.config) {
-            Object.assign(config, this.props.config);
-        }
-        
-        this.service = AWSIoTData[this.type](config);
-        
-        if (this.props.onConnect) {
-            this.service.on('connect', this.props.onConnect);
-        }
-        
-        if (this.props.onReconnect) {
-            this.service.on('reconnect', this.props.onReconnect);
-        }
-        
-        if (this.props.onDelta) {
-            this.service.on('delta', this.props.onDelta);
-        }
-        
-        if (this.props.onStatus) {
-            this.service.on('status', this.props.onStatus);
-        }
-        
-        if (this.props.onClose) {
-            this.service.on('close', this.props.onClose);
-        }
-        
-        if (this.props.onOffline) {
-            this.service.on('offline', this.props.onOffline);
-        }
-    }
-    
-    addThing(thingId, extraConfig) {
-        if (this.type === 'device') {
-            console.warn('addthing is only supported for shadows implementations');
-            return;
-        }
-        if (this.registry[thingId]) {
-            return;
-        }
-        const config = {
-            persistentSubscribe: true
-        };
-        if (extraConfig) {
-            Object.assign(config, extraConfig);
-        }
-        this.registry[thingId] = config;
-        let callback = null;
-        if (this.props.onThingConnected) {
-            callback = this.props.onThingConnected.bind(null, thingId);
-        }
-        this.service.register(thingId, config, callback);
-        this.service.on('close', () => {
-            delete this.registry[thingId];
-            this.service.unregister(thingId);
-        });
-    }
-    
-    render() {
-        return null;
-    }
-}
+// import AWS from 'aws-sdk/dist/aws-sdk-react-native';
+import {AWSIoTData} from './aws-iot-device-sdk-js-react-native.js';
 
 /**
  * Use this class with redux
  *
- * @param configuration
- * @returns {{ AWSIoTMQTTClient }}
+ * @param {Object} configuration
+ * @return {{ AWSIoTMQTTClient }}
  * @constructor
  */
-export function AWSIoTMQTTClient(newConfig) {
+export function AWSIoTMQTTClient(configuration) {
     const self = { };
     self.service = null;
     self.store = null;
-    newConfig.type = newConfig.type === 'shadow' ? 'thingShadow' : 'device';
-    
+    configuration.type = configuration.type === 'shadow' ? 'thingShadow' : 'device';
+
     const config = {
         //
         // Set the AWS region we will operate in.
         //
-        region: newConfig.region,
-        host: newConfig.host,
+        region: configuration.region,
+        host: configuration.host,
         //
         // Connect via secure WebSocket
         //
@@ -138,61 +29,82 @@ export function AWSIoTMQTTClient(newConfig) {
         // so we don't want to leave the user waiting too long for reconnection after
         // re-connecting to the network/re-opening their laptop/etc...
         //
-        maximumReconnectTimeMs: 8000,
+        maximumReconnectTimeMs: 1000,
         //
         // Enable console debugging information (optional)
         //
-        debug: false,
+        debug: true,
         //
         // IMPORTANT: the AWS access key ID, secret key, and sesion token must be
         // initialized with empty strings.
         //
         accessKeyId: '',
         secretKey: '',
-        sessionToken: ''
+        sessionToken: '',
     };
-    
-    if (newConfig) {
-        Object.assign(config, newConfig);
+
+    if (configuration) {
+        Object.assign(config, configuration);
     }
-    
-    self.connect = function (accessKeyId, secretAccessKey, sessionToken) {
+
+    self.connect = function(accessKeyId, secretAccessKey, sessionToken) {
         config.accessKeyId = accessKeyId;
         config.secretKey = secretAccessKey;
         config.sessionToken = sessionToken;
         self.service = AWSIoTData[config.type](config);
+        self.config = config;
         self.service.on('connect', self.onConnect);
         self.service.on('reconnect', self.onReconnect);
+        self.service.on('offline', self.onOffline);
+        self.service.on('error', self.onError);
+        self.service.on('message', self.onMessage);
         self.service.on('status', self.onStatus);
         self.service.on('delta', self.onDelta);
         self.service.on('close', self.onClose);
+        self.service.on('timeout', self.onTimeout);
     };
-    
-    self.setStore = function (store) {
+
+    self.setStore = function(store) {
         self.store = store;
     };
-    
-    self.onConnect = function (test) {
-        self.store.dispatch({ type: 'MQTT_ON_CONNECT', test });
+
+    self.onConnect = function() {
+        self.store.dispatch({type: 'MQTT_ON_CONNECT'});
     };
-    
-    self.onReconnect = function (test) {
-        self.store.dispatch({ type: 'MQTT_ON_RECONNECT', test });
+
+    self.onReconnect = function() {
+        self.store.dispatch({type: 'MQTT_ON_RECONNECT'});
     };
-    
-    self.onStatus = function (thingId, statusType, clientToken, stateObject) {
-        self.store.dispatch({ type: 'MQTT_ON_STATUS', thingId, statusType, clientToken, stateObject });
+
+    self.onOffline = function() {
+        self.store.dispatch({type: 'MQTT_ON_OFFLINE'});
     };
-    
-    self.onDelta = function (thingName, stateObject) {
-        self.store.dispatch({ type: 'MQTT_ON_DELTA', thingName, stateObject });
+
+    self.onError = function(error) {
+        self.store.dispatch({type: 'MQTT_ON_ERROR', error});
     };
-    
-    self.onClose = function (test) {
-        self.store.dispatch({ type: 'MQTT_ON_CLOSE', test });
+
+    self.onMessage = function(topic, payload) {
+        self.store.dispatch({type: 'MQTT_ON_MESSAGE', topic, payload});
     };
-    
+
+    self.onStatus = function(thingId, statusType, clientToken, stateObject) {
+        self.store.dispatch({type: 'MQTT_ON_STATUS', thingId, statusType, clientToken, stateObject});
+    };
+
+    self.onDelta = function(thingName, stateObject) {
+        self.store.dispatch({type: 'MQTT_ON_DELTA', thingName, stateObject});
+    };
+
+    self.onClose = function() {
+        self.store.dispatch({type: 'MQTT_ON_CLOSE'});
+    };
+
+    self.onTimeout = function(thingName, clientToken) {
+        self.store.dispatch({type: 'MQTT_ON_TIMEOUT', thingName, clientToken});
+    };
+
     return self;
 }
 
-export default AWSIoTMQTT;
+export default AWSIoTMQTTClient;
